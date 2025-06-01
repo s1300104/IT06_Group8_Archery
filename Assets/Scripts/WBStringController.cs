06bc40811ad9cfa5c787d0e8fdd2c743c18474a6
s1300141 <116261708+mell330@users.noreply.github.com>
2025-06-01 20:30:21 +0900

    pull the arrow

--- Assets/Scripts/WBStringController.cs
+++ Assets/Scripts/WBStringController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class WBStringController : MonoBehaviour
 {
@@ -8,41 +9,60 @@ public class WBStringController : MonoBehaviour
     Vector3 BowHandGpos;
     Vector3 BowGrabGpos;         // 弓の持ち手の位置を格納
     Vector3 BowGrabLpos;
+    private XRGrabInteractable grabInteractable;
     private bool isGrabbing = false;
+    private ParentScript parentScript;
     //Vector3 BowStringPos;   // 弓の弦の位置を格納
     //Vector3 BowTopPos;      // 弓の末弭の位置を格納
     //Vector3 BowButtomPos;   // 弓の本弭の位置を格納
     // Start is called before the first frame update
     void Start()
     {
-        
+        grabInteractable = GetComponent<XRGrabInteractable>();
+
+        // 親オブジェクトにアタッチされているParentScriptのインスタンスを取得
+        parentScript = GetComponentInParent<ParentScript>();
+        if (parentScript == null)
+        {
+            Debug.LogWarning("親オブジェクトにParentScriptが見つかりませんでした。");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E))
+        
+        if (grabInteractable.isSelected)
         {
             isGrabbing = true;
-            Debug.Log("掴み開始");
+            Debug.Log("オブジェクトが掴まれています！");
         }
-        if (Input.GetKeyUp(KeyCode.E)) // Eキーを離すと掴むのをやめる
+        else
         {
             isGrabbing = false;
-            Debug.Log("掴み終了");
+            Debug.Log("オブジェクトは掴まれていません。");
         }
-
-        // 掴んでいる間の処理
-        if (isGrabbing)    
+        
+        if(isGrabbing)
         {
-            BowHandGpos = GameObject.Find("Attach_string").transform.position;
-            BowGrabGpos = GameObject.Find("Attach_bow").transform.position;
-            BowGrabLpos = GameObject.Find("Attach_bow").transform.localPosition;
+            if (parentScript != null)
+            {
+                BowHandGpos = GameObject.Find("Attach_string").transform.position;
+                BowGrabGpos = GameObject.Find("Attach_bow").transform.position;
+                BowGrabLpos = GameObject.Find("Attach_bow").transform.localPosition;
 
-            float newY = Vector3.Dot(BowHandGpos-BowGrabGpos, new Vector3(0.0f, BowGrabLpos.y, 0.0f));
+                float newY = Vector3.Dot(BowHandGpos-BowGrabGpos, new Vector3(0.0f, BowGrabLpos.y, 0.0f));
 
-            transform.localPosition = new Vector3(0.0f, newY, 0.0f);
+                //transform.localPosition = ;
+                // ParentScriptの公開メソッドを呼び出して親の座標を変更
+                parentScript.MoveParent(new Vector3(0.0f, newY, 0.0f));
+            }
         }
+        // 掴んでいる間の処理
+        //if (isGrabbing)    
+        //{
+            
+        //}
         /*
         ArrowPos = GameObject.Find("Attach_arrow").transform.localPosition;
         BowGrabPos = GameObject.Find("Attach_bow").transform.localPosition;
